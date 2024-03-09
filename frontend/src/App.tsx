65c03b4f1ea2026ddac1f225729a4d97b6311bfc
Juuso Finne <juuso.finne@tuni.fi>
2024-03-09 11:04:09 +0200

    Extended color palette

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
 
   return (
     <>
+
     </>
   )
 }
--- frontend/src/Components/GameCard.tsx
+++ frontend/src/Components/GameCard.tsx
@@ -6,7 +6,7 @@ type props = {
 const GameCard = ({ game }: props) => {
     const { name, description, image } = game
     return (
-        <div className="bg-teal-800 rounded-md text-white p-4 h-[18rem] w-[11rem] overflow-hidden">
+        <div className="bg-bice-blue rounded-md text-white p-4 h-[18rem] w-[11rem] overflow-hidden">
             <div className="h-1/4 flex flex-row justify-between gap-2 mb-2">
                 <h1 className="text-left flex w-3/4 font-bold p-2">{name}</h1>
                 <img src={image} className="object-fit  p-2" />
