7a36063bd56d91016f89f2ab6a8ad2bb2a2a52ac
Juuso Finne <juuso.finne@tuni.fi>
2024-03-17 15:25:25 +0200

    Made review component more consisten with page

--- frontend/src/Components/GameReview.tsx
+++ frontend/src/Components/GameReview.tsx
@@ -8,8 +8,8 @@ const GameReview = ({ review }: props) => {
     return (
         <div className="my-5">
             <div className="bg-bice-blue flex flex-row max-[350px]:flex-col justify-between p-5">
-                <p className="text-4xl">Review by: {userName}</p>
-                <p className="text-7xl">{score}</p>
+                <p className="text-2xl sm:text-4xl mr-3">Review by: {userName}</p>
+                <p className="text-7xl max-md:text-4xl">{score}</p>
             </div>
             <div className="bg-picton-blue text-base p-5">
                 {reviewText}
