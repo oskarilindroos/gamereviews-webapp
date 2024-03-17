bd025ad81353bfec7d288a6be03321291bb93fe1
Juuso Finne <juuso.finne@tuni.fi>
2024-03-17 13:01:05 +0200

    Added placeholder image

--- frontend/src/Pages/GameReviewPage.tsx
+++ frontend/src/Pages/GameReviewPage.tsx
@@ -21,15 +21,75 @@ const dummyData: GameReviewData[] = [
 
 const GameReviewPage = () => {
     const { gameId } = useParams();
-    // TODO: Fetch actual reviews using gameId
+    // TODO: Fetch actual reviews and gameInfo using gameId
+    const gameInfo = {
+        name: "Zombies shat on my brains",
+        tags: ["Action", "Horror", "Yet another indie game"],
+        description: "Lorem ipsum dolor sit amet. Est illo sint est mollitia nobis et esse quibusdam qui quisquam necessitatibus est doloremque molestiae. Et ipsum voluptatem qui quia assumenda non odit vitae ut eveniet ipsa aut autem quos et nobis voluptatibus!",
+        image: "https://images.pexels.com/photos/275033/pexels-photo-275033.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    }
 
     return (
-        <div className="bg-indigo-dye text-gray-100 font-mono w-screen h-screen flex justify-center overflow-auto">
+        <div className="bg-indigo-dye text-gray-100 font-mono w-screen h-screen flex flex-col items-center overflow-auto">
+
+            <div className="w-10/12 flex flex-col py-10 grow">
+                <div className="flex flex-row max-xl:flex-col">
+
+                    <div className="flex w-5/12 max-xl:hidden mr-3">
+                        <img src={gameInfo.image} className="object-cover"></img>
+                    </div>
+
+
+                    <div className="flex flex-col w-7/12 max-xl:w-full ">
+
+                        <div className="bg-bice-blue h-1/3 p-5 flex items-center justify-center">
+                            <p className="truncate text-sm sm:text-4xl">{gameInfo.name}</p>
+                        </div>
+
+                        <div className="h-1/3 flex items-center p-5">
+                            <p className="text-7xl max-xl:text-4xl">
+                                {averageScore(dummyData)}
+                            </p>
+                        </div>
+
+                        <div className="h-1/3 flex items-center p-5">
+                            <p className="text-sm sm:text-base truncate">
+                                {gameInfo.tags.map((tag, index) => <span key={index} > {` [${tag}] `} </span>)}
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="mt-3">
+                    <p className="text-base">
+                        {gameInfo.description}
+                    </p>
+                </div>
+            </div>
+
+            {/* TODO: Wrap in a Link leading to write review page */}
+            <div className="bg-bice-blue mb-10 w-10/12 text-center">
+                <p className="text-4xl">Leave a review</p>
+            </div>
+
             <div className="w-10/12">
                 {dummyData.map((item, index) => <GameReview key={index} review={item} />)}
             </div>
+
         </div>
     )
+}
+
+function averageScore(reviews: GameReviewData[]): number {
+    let average = 0;
+    if (reviews.length !== 0) {
+        reviews.map(item => average += item.score);
+        average /= reviews.length;
+    }
+    // Round to two decimal places
+    average = Math.round(average * 100) / 100
+    return average;
 }
 
 export default GameReviewPage
