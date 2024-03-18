7af24d7db639917b6c20ad2668375de176243d03
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-03-18 02:44:36 +0200

    SearchPage updated

--- frontend/src/Pages/SearchPage.tsx
+++ frontend/src/Pages/SearchPage.tsx
@@ -14,18 +14,70 @@ const SearchPage = () => {
     return (
         <>
             <div className="mt-40">
-                <div className="flex flex-row pr-14">
+                <div className="flex flex-row">
 
+                    <div className="pr-14">
                     <DropdownMenu name={"order"} content={testArray}></DropdownMenu>
                     <DropdownMenu name={"tags"} content={testArray}></DropdownMenu>
                     <DropdownMenu name={"year"} content={testArray}></DropdownMenu>
                     <DropdownMenu name={"rating"} content={testArray}></DropdownMenu>
-                    <SearchBar onSearch={handleSearch} />
+                    </div>
+
+                    <div className="flex flex-row ml-auto">
+                        <SearchBar onSearch={handleSearch} />
+                    </div>
+
 
 
                 </div>
             </div>
-            <div className="mt-20">
+            <div className="mt-4">
+                <div className="flex flex-col items-center">
+                    <ul className="flex flex-row items-baseline mt-8">
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                    </ul>
+                    <div className="mb-2"></div>
+                </div>
+                <div className="flex flex-col items-center">
+                    <ul className="flex flex-row items-baseline mt-8">
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                        <li>
+                            <img className="h-80 mx-4" src={Poster}></img>
+                        </li>
+                    </ul>
+                    <div className="mb-2"></div>
+                </div>
                 <div className="flex flex-col items-center">
                     <ul className="flex flex-row items-baseline mt-8">
                         <li>
