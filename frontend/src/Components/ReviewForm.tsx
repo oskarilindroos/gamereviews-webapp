d4ad4268ed8aa6c1997f009e93786acc9b493f04
Juuso Finne <juuso.finne@tuni.fi>
2024-03-24 20:21:29 +0200

    ReviewForm: Made the default score 3, made it impossible to submit empty review

--- frontend/src/Components/ReviewForm.tsx
+++ frontend/src/Components/ReviewForm.tsx
@@ -18,7 +18,7 @@ const ReviewForm = ({ submitHandler }: props) => {
                     {/* Create a dropdown with 5 elements */}
                     <select {...register("score")} className="bg-bice-blue py-2 px-4 rounded-md">
                         {[...Array(5)].map((_, n) => (
-                            <option key={n + 1} value={n + 1}>
+                            <option key={n + 1} value={n + 1} selected={n + 1 === 3}>
                                 {n + 1}
                             </option>
                         ))}
@@ -30,7 +30,7 @@ const ReviewForm = ({ submitHandler }: props) => {
                 </div>
 
                 <div>
-                    <textarea rows={5} className="w-full bg-bice-blue" {...register("reviewText")} />
+                    <textarea rows={5} className="w-full bg-bice-blue" {...register("reviewText", { required: true })} />
                 </div>
 
                 <div className="my-2">
