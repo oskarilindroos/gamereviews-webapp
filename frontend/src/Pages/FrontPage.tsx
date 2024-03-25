6583f5cf8cdb19d8bf41850e8a1690f0b0061638
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-03-25 12:10:59 +0200

    #30
    -the image now scales with width instead of height

--- frontend/src/Pages/FrontPage.tsx
+++ frontend/src/Pages/FrontPage.tsx
@@ -53,7 +53,7 @@ const FrontPage = () => {
             <div className="mt-40">
                 <div className="flex flex-row">
                     <h1 className="font-mono text-gray-100 text-8xl">Discover<br />And Review<br />Games</h1>
-                    <img className="h-96 ml-auto" src={Laptop}></img>
+                    <img className="w-4/12 ml-auto" src={Laptop}></img>
                 </div>
             </div>
             <div className="mt-40">
