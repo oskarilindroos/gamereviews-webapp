ffff768fc69c5c7f465c90d91e8d48478e1dab8e
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-03-25 11:07:25 +0200

    #29 done
    -added testGames thing here as well
    -replaced hardcoded list with PosterRow

--- frontend/src/Pages/FrontPage.tsx
+++ frontend/src/Pages/FrontPage.tsx
@@ -1,7 +1,53 @@
 import Laptop from '../Assets/laptop.png'
 import Poster from '../Assets/poster_template.png'
+import PosterRow from '../Components/PosterRow';
+import { GameSummary } from '../Types';
 
 const FrontPage = () => {
+
+    let testGames: GameSummary[] = [{
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    },
+    {
+        image: "https://newbloodstore.com/cdn/shop/products/NBPosters_DUSK-NoBorder_2021_1024x1024.jpg?v=1644573550",
+        name: "Dusk",
+        description: "game"
+    }
+    ];
+
+
     return (
         <>
             <div className="mt-40">
@@ -14,30 +60,7 @@ const FrontPage = () => {
                 <div className="flex flex-col items-center">
                     <h1 className="font-mono text-gray-100 text-4xl self-center">currently trending</h1>
                     <ul className="flex flex-row items-baseline mt-8">
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
-                        <li>
-                            <img className="h-64 mx-4" src={Poster}></img>
-                        </li>
+                        <PosterRow games={testGames} page={"FrontPage"}></PosterRow>
                     </ul>
                     <div className="mb-40"></div>
                 </div>
