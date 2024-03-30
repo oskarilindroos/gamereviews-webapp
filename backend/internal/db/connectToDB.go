6b5fdb2f2c1ed43f6ff01fd3ab9d9d4eaa683e9f
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-03-30 20:22:41 +0200

    formatted file

--- backend/internal/db/connectToDB.go
+++ backend/internal/db/connectToDB.go
@@ -9,15 +9,15 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
-func ConnectToDB() (*sql.DB, error){
+func ConnectToDB() (*sql.DB, error) {
 	var db *sql.DB
 	// Capture connection properties
 	cfg := mysql.Config{
-		User:   os.Getenv("DB_USER"),
-		Passwd: os.Getenv("DB_PASSWORD"),
-		Net: os.Getenv("DB_NET"),
-		Addr: os.Getenv("DB_ADDR"),
-		DBName: os.Getenv("DB_DATABASE"),
+		User:      os.Getenv("DB_USER"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       os.Getenv("DB_NET"),
+		Addr:      os.Getenv("DB_ADDR"),
+		DBName:    os.Getenv("DB_DATABASE"),
 	}
 
 	// Get database handle
@@ -25,7 +25,7 @@ func ConnectToDB() (*sql.DB, error){
 	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
-		return nil,err
+		return nil, err
 	}
 
 	pingErr := db.Ping()
@@ -35,5 +35,6 @@ func ConnectToDB() (*sql.DB, error){
 	}
 	fmt.Println("connected!")
 
-	return db,nil
+	return db, nil
 }
+
