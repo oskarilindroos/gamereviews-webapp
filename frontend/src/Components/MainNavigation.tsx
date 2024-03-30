6c977a26bd173b356978226db0358bc8b9d4c24a
Juuso Finne <juuso.finne@tuni.fi>
2024-03-30 13:44:57 +0200

    #54 Added 'My reviews' Link to navigation bar

--- frontend/src/Components/MainNavigation.tsx
+++ frontend/src/Components/MainNavigation.tsx
@@ -18,6 +18,15 @@ const MainNavigation = () => {
                                 <h1 className="font-mono text-gray-100 text-4xl ml-10">games</h1>
                             </Link>
                         </li>
+
+                        <li>
+                            {/* TODO: Replace :userId with actual userId */}
+
+                            {/* TODO: Make visible to logged in users only */}
+                            <Link to="/user/:userId/reviews">
+                                <h1 className="font-mono text-gray-100 text-4xl ml-10">my reviews</h1>
+                            </Link>
+                        </li>
                         <li>
                             <Link to="/">
                                 <h1 className="font-mono text-gray-100 text-4xl ml-10">sign in</h1>
