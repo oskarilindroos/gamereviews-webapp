429a22eb3fd968dc5c2cd7643eea3a6fd6aa3525
Oskari Lindroos <oskarilindroos00@gmail.com>
2024-04-26 20:44:54 +0300

    formatted

--- backend/internal/games/games_handler_test.go
+++ backend/internal/games/games_handler_test.go
@@ -18,13 +18,12 @@ import (
 	"github.com/oskarilindroos/review-app/internal/models"
 )
 
-const(
-	testGetGameByIdHandlerWithReviews string ="test_data/games_handler_get_game_by_ID_with_reviews.json"
-	testGetGameByIdHandlerNoReviews string ="test_data/games_handler_get_game_by_ID_no_reviews.json"
-
+const (
+	testGetGameByIdHandlerWithReviews string = "test_data/games_handler_get_game_by_ID_with_reviews.json"
+	testGetGameByIdHandlerNoReviews   string = "test_data/games_handler_get_game_by_ID_no_reviews.json"
 )
 
-func TestGetGamesHandler(t *testing.T){
+func TestGetGamesHandler(t *testing.T) {
 
 	t.Setenv("TWITCH_CLIENT_ID", "08pq42o75ypbgn28lkygv407r9apgr")
 	t.Setenv("IGDB_TOKEN_TILL_17_05", "69ai7xb1ccdubbj0qehfnufzcfx8yp")
@@ -33,7 +32,7 @@ func TestGetGamesHandler(t *testing.T){
 	if err != nil {
 		t.Fatal(err)
 	}
-	single := make([]*models.GamesList,0)
+	single := make([]*models.GamesList, 0)
 	err = json.Unmarshal(f1, &single)
 	if err != nil {
 		t.Fatal(err)
@@ -43,52 +42,52 @@ func TestGetGamesHandler(t *testing.T){
 	if err != nil {
 		t.Fatal(err)
 	}
-	multi := make([]*models.GamesList,0)
+	multi := make([]*models.GamesList, 0)
 	err = json.Unmarshal(f2, &multi)
 	if err != nil {
 		t.Fatal(err)
 	}
-	handler:= games.GamesHandler{}
-	
-	var tests = []struct{
-		name 				string
-		page_number 		string
-		number_of_games 	string
-		order 				string
-		order_by 			string
-		wantSuccessBody		[]*models.GamesList
-		wantErrorBody		string
-		wantCode			int
+	handler := games.GamesHandler{}
+
+	var tests = []struct {
+		name            string
+		page_number     string
+		number_of_games string
+		order           string
+		order_by        string
+		wantSuccessBody []*models.GamesList
+		wantErrorBody   string
+		wantCode        int
 	}{
-		{"get single game with status ok","1","1","desc","relevance",single,"", http.StatusOK},
-		{"get list of games with status ok","1","10","asc","id",multi,"", http.StatusOK},
-		{"Get error empty variables","","","","",nil,
-		`{"error":"cannot get index of Games: cannot create request with invalid options: cannot unwrap invalid option: cannot compose invalid functional options: cannot unwrap invalid option: one or more provided option field values are empty"}`,
-		http.StatusInternalServerError},
-		{"Get error number out of range","1","-1","asc","hypes",nil,
-		`{"error":"Page number was lower than 1 needs to be 1 or higher"}`,
-		http.StatusInternalServerError},
+		{"get single game with status ok", "1", "1", "desc", "relevance", single, "", http.StatusOK},
+		{"get list of games with status ok", "1", "10", "asc", "id", multi, "", http.StatusOK},
+		{"Get error empty variables", "", "", "", "", nil,
+			`{"error":"cannot get index of Games: cannot create request with invalid options: cannot unwrap invalid option: cannot compose invalid functional options: cannot unwrap invalid option: one or more provided option field values are empty"}`,
+			http.StatusInternalServerError},
+		{"Get error number out of range", "1", "-1", "asc", "hypes", nil,
+			`{"error":"Page number was lower than 1 needs to be 1 or higher"}`,
+			http.StatusInternalServerError},
 	}
 
-	for _, test := range tests{
+	for _, test := range tests {
 
-		t.Run(test.name,func(t *testing.T) {
-			resp:=httptest.NewRecorder()
-		
-			req := httptest.NewRequest("GET","/",nil)
+		t.Run(test.name, func(t *testing.T) {
+			resp := httptest.NewRecorder()
+
+			req := httptest.NewRequest("GET", "/", nil)
 
 			q := req.URL.Query()
-			q.Add("page_number",test.page_number)
-			q.Add("number_of_games",test.number_of_games)
-			q.Add("order",test.order)
-			q.Add("order_by",test.order_by)
-			req.URL.RawQuery= q.Encode()
+			q.Add("page_number", test.page_number)
+			q.Add("number_of_games", test.number_of_games)
+			q.Add("order", test.order)
+			q.Add("order_by", test.order_by)
+			req.URL.RawQuery = q.Encode()
 			handler.GetGamesHandler(resp, req)
 
 			if resp.Result().StatusCode != test.wantCode {
-				t.Fatalf("The status code should be <%v> but received <%v>",test.wantCode,resp.Result().StatusCode)
+				t.Fatalf("The status code should be <%v> but received <%v>", test.wantCode, resp.Result().StatusCode)
 			}
-			if resp.Result().StatusCode == http.StatusOK{
+			if resp.Result().StatusCode == http.StatusOK {
 
 				bb := resp.Body.Bytes()
 				c := []*models.GamesList{}
@@ -97,23 +96,23 @@ func TestGetGamesHandler(t *testing.T){
 				if err != nil {
 					t.Fatal(err)
 				}
-				if !reflect.DeepEqual(test.wantSuccessBody,c){
-					t.Fatalf("Wanted in body: <%v> \ngot: <%v>",test.wantSuccessBody,c)
+				if !reflect.DeepEqual(test.wantSuccessBody, c) {
+					t.Fatalf("Wanted in body: <%v> \ngot: <%v>", test.wantSuccessBody, c)
 				}
-			}else{
+			} else {
 				bb := resp.Body.String()
 				cc := test.wantErrorBody
-				d :=  strings.TrimSpace(bb)
-				if d != cc{
-					t.Fatalf("Wanted error message '<%v>' \n got '<%v>'",cc,d)
+				d := strings.TrimSpace(bb)
+				if d != cc {
+					t.Fatalf("Wanted error message '<%v>' \n got '<%v>'", cc, d)
 				}
 			}
-			
+
 		})
 	}
 }
 
-func TestSearchGamesHandler(t *testing.T){
+func TestSearchGamesHandler(t *testing.T) {
 
 	t.Setenv("TWITCH_CLIENT_ID", "08pq42o75ypbgn28lkygv407r9apgr")
 	t.Setenv("IGDB_TOKEN_TILL_17_05", "69ai7xb1ccdubbj0qehfnufzcfx8yp")
@@ -122,47 +121,47 @@ func TestSearchGamesHandler(t *testing.T){
 	if err != nil {
 		t.Fatal(err)
 	}
-	init := make([]*models.GamesList,0)
+	init := make([]*models.GamesList, 0)
 	err = json.Unmarshal(f, &init)
 	if err != nil {
 		t.Fatal(err)
 	}
-	handler:= games.GamesHandler{}
-	
-	var tests = []struct{
-		name 				string
-		page_number 		string
-		number_of_games 	string
-		search 				string
-		wantSuccessBody		[]*models.GamesList
-		wantErrorBody		string
-		wantCode			int
+	handler := games.GamesHandler{}
+
+	var tests = []struct {
+		name            string
+		page_number     string
+		number_of_games string
+		search          string
+		wantSuccessBody []*models.GamesList
+		wantErrorBody   string
+		wantCode        int
 	}{
-		{"search games with status ok","1","3","zelda",init,"", http.StatusOK},
-		{"Get error empty variables","","","",nil, `{"error":"Did not give search parameters"}`,400},
-		{"Get error number out of range","1","-1","zelda",nil, `{"error":"Page number was lower than 1 needs to be 1 or higher"}`, http.StatusInternalServerError},
+		{"search games with status ok", "1", "3", "zelda", init, "", http.StatusOK},
+		{"Get error empty variables", "", "", "", nil, `{"error":"Did not give search parameters"}`, 400},
+		{"Get error number out of range", "1", "-1", "zelda", nil, `{"error":"Page number was lower than 1 needs to be 1 or higher"}`, http.StatusInternalServerError},
 	}
 
-	for _, test := range tests{
+	for _, test := range tests {
 
-		t.Run(test.name,func(t *testing.T) {
-			resp:=httptest.NewRecorder()
-		
-			req := httptest.NewRequest("GET","/search",nil)
+		t.Run(test.name, func(t *testing.T) {
+			resp := httptest.NewRecorder()
+
+			req := httptest.NewRequest("GET", "/search", nil)
 
 			q := req.URL.Query()
-			q.Add("page_number",test.page_number)
-			q.Add("number_of_games",test.number_of_games)
-			q.Add("search_content",test.search)
-			req.URL.RawQuery= q.Encode()
+			q.Add("page_number", test.page_number)
+			q.Add("number_of_games", test.number_of_games)
+			q.Add("search_content", test.search)
+			req.URL.RawQuery = q.Encode()
 			handler.SearchGamesHandler(resp, req)
 
 			if resp.Result().StatusCode != test.wantCode {
-				log.Printf("status: %v",resp.Body.String())
-				t.Fatalf("The status code should be <%v> but received <%v>",test.wantCode,resp.Result().StatusCode)
-			
+				log.Printf("status: %v", resp.Body.String())
+				t.Fatalf("The status code should be <%v> but received <%v>", test.wantCode, resp.Result().StatusCode)
+
 			}
-			if resp.Result().StatusCode == http.StatusOK{
+			if resp.Result().StatusCode == http.StatusOK {
 
 				bb := resp.Body.Bytes()
 				c := []*models.GamesList{}
@@ -171,33 +170,33 @@ func TestSearchGamesHandler(t *testing.T){
 				if err != nil {
 					t.Fatal(err)
 				}
-				if !reflect.DeepEqual(test.wantSuccessBody,c){
-					t.Fatalf("Wanted in body: <%v> \ngot: <%v>",test.wantSuccessBody,c)
+				if !reflect.DeepEqual(test.wantSuccessBody, c) {
+					t.Fatalf("Wanted in body: <%v> \ngot: <%v>", test.wantSuccessBody, c)
 				}
-			}else{
+			} else {
 				bb := resp.Body.String()
 				cc := test.wantErrorBody
-				d :=  strings.TrimSpace(bb)
-				if d != cc{
-					t.Fatalf("Wanted error message '<%v>' \n got '<%v>'",cc,d)
+				d := strings.TrimSpace(bb)
+				if d != cc {
+					t.Fatalf("Wanted error message '<%v>' \n got '<%v>'", cc, d)
 				}
 			}
-			
+
 		})
 	}
 }
 
-func TestGetGameByIdHandler(t *testing.T){
+func TestGetGameByIdHandler(t *testing.T) {
 
 	t.Setenv("TWITCH_CLIENT_ID", "08pq42o75ypbgn28lkygv407r9apgr")
 	t.Setenv("IGDB_TOKEN_TILL_17_05", "69ai7xb1ccdubbj0qehfnufzcfx8yp")
-	t.Setenv("DB_PASSWORD","dev")
-	t.Setenv("CORS_ORIGIN","http://localhost:5173")
-	t.Setenv("PORT","5050")
-	t.Setenv("DB_USER","dev")
-	t.Setenv("DB_NET","tcp")
-	t.Setenv("DB_ADDR","localhost")
-	t.Setenv("DB_DATABASE","reviewapp")
+	t.Setenv("DB_PASSWORD", "dev")
+	t.Setenv("CORS_ORIGIN", "http://localhost:5173")
+	t.Setenv("PORT", "5050")
+	t.Setenv("DB_USER", "dev")
+	t.Setenv("DB_NET", "tcp")
+	t.Setenv("DB_ADDR", "localhost")
+	t.Setenv("DB_DATABASE", "reviewapp")
 
 	db, err := db.ConnectToDB()
 	if err != nil {
@@ -214,7 +213,7 @@ func TestGetGameByIdHandler(t *testing.T){
 	games.SetupRoutes(r, gamesHandler) // Setup /api/games routes
 	r.Use(middleware.Cors)
 
-	type b struct{	
+	type b struct {
 		ID      int       `json:"reviewId" db:"id"`
 		IGDBID  string    `json:"igdbId" db:"igdb_id"`
 		UserID  *string   `json:"userId" db:"user_id"` // WARN: Nullable
@@ -223,18 +222,17 @@ func TestGetGameByIdHandler(t *testing.T){
 		Created time.Time `json:"createdAt" db:"created"`
 		Updated time.Time `json:"updatedAt" db:"updated"`
 	}
-	type a struct{
-		GameID      int                  `json:"igdbId"`
-		Name        string               `json:"name"`
-		Cover       string               `json:"coverUrl"`
-		AgeRating   string               `json:"ageRating"`
-		ReleaseDate int                  `json:"releaseDate"`
-		Genres      string               `json:"genres"`
-		Storyline   string               `json:"storyline"`
-		Summary     string               `json:"summary"`
-		Reviews     []b		 			`json:"reviews"`
+	type a struct {
+		GameID      int    `json:"igdbId"`
+		Name        string `json:"name"`
+		Cover       string `json:"coverUrl"`
+		AgeRating   string `json:"ageRating"`
+		ReleaseDate int    `json:"releaseDate"`
+		Genres      string `json:"genres"`
+		Storyline   string `json:"storyline"`
+		Summary     string `json:"summary"`
+		Reviews     []b    `json:"reviews"`
 	}
-
 
 	f, err := os.ReadFile(testGetGameByIdHandlerWithReviews)
 	if err != nil {
@@ -254,44 +252,38 @@ func TestGetGameByIdHandler(t *testing.T){
 	if err != nil {
 		t.Fatal(err)
 	}
-	
-	
-	var tests = []struct{
-		name 				string
-		gameID				string
-		wantSuccessBody		*a
-		wantErrorBody		string
-		wantCode			int
+
+	var tests = []struct {
+		name            string
+		gameID          string
+		wantSuccessBody *a
+		wantErrorBody   string
+		wantCode        int
 	}{
-		{"Get game data successfully","131913",withReviews,"",http.StatusOK},
-		{"Get game data successfully","13191",noReviews,"",http.StatusOK},
-		{"Invalid ID","-1",nil,`{"error":"ID cannot be negative"}`, http.StatusInternalServerError},
-		{"Zero Id","0",nil,`{"error":"cannot get Game with ID 0: cannot make POST request: results are empty"}`, http.StatusInternalServerError},
+		{"Get game data successfully", "131913", withReviews, "", http.StatusOK},
+		{"Get game data successfully", "13191", noReviews, "", http.StatusOK},
+		{"Invalid ID", "-1", nil, `{"error":"ID cannot be negative"}`, http.StatusInternalServerError},
+		{"Zero Id", "0", nil, `{"error":"cannot get Game with ID 0: cannot make POST request: results are empty"}`, http.StatusInternalServerError},
 	}
 
-	for _, test := range tests{
+	for _, test := range tests {
 
-		t.Run(test.name,func(t *testing.T) {
-			resp:=httptest.NewRecorder()
+		t.Run(test.name, func(t *testing.T) {
+			resp := httptest.NewRecorder()
 
-			
+			req := httptest.NewRequest("GET", "/{igdbId}", nil)
+			req = mux.SetURLVars(req, map[string]string{"igdbId": test.gameID})
 
-
-
-			req:= httptest.NewRequest("GET","/{igdbId}",nil)
-			req = mux.SetURLVars(req,map[string]string{"igdbId":test.gameID})
-	
 			q := req.URL.Query()
-			req.URL.RawQuery= q.Encode()
-			
+			req.URL.RawQuery = q.Encode()
+
 			gamesHandler.GetGameByIdHandler(resp, req)
 
-
 			if resp.Result().StatusCode != test.wantCode {
-				t.Fatalf("The status code should be <%v> but received <%v>",test.wantCode,resp.Result().StatusCode)
-			
+				t.Fatalf("The status code should be <%v> but received <%v>", test.wantCode, resp.Result().StatusCode)
+
 			}
-			if resp.Result().StatusCode == http.StatusOK{
+			if resp.Result().StatusCode == http.StatusOK {
 
 				bb := resp.Body.Bytes()
 				c := &a{}
@@ -300,18 +292,18 @@ func TestGetGameByIdHandler(t *testing.T){
 				if err != nil {
 					t.Fatal(err)
 				}
-				if !reflect.DeepEqual(test.wantSuccessBody,c){
-					t.Fatalf("Wanted in body: <%v> \ngot: <%v>",test.wantSuccessBody,c)
+				if !reflect.DeepEqual(test.wantSuccessBody, c) {
+					t.Fatalf("Wanted in body: <%v> \ngot: <%v>", test.wantSuccessBody, c)
 				}
-			}else{
+			} else {
 				bb := resp.Body.String()
 				cc := test.wantErrorBody
-				d :=  strings.TrimSpace(bb)
-				if d != cc{
-					t.Fatalf("Wanted error message '<%v>' \n got '<%v>'",cc,d)
+				d := strings.TrimSpace(bb)
+				if d != cc {
+					t.Fatalf("Wanted error message '<%v>' \n got '<%v>'", cc, d)
 				}
 			}
-			
+
 		})
 	}
 }
